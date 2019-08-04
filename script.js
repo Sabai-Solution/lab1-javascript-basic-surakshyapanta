475d467046f0402f35090205caf68b0efa2c6465
Surakshya Panta <pantasurakshya5@gmail.com>
2019-08-04 10:01:21 +0545

    Make empty validation work

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-function ValidateEmptyValues(){
-var txtEmail=document.getElementById("txtEmail").value;
+function validateEmptyValues(){
+
+var txtEmail = document.getElementById("txtEmail").value;
+var txtUn = document.getElementById("txtUn").value;
+var txtPassword = document.getElementById("txtPassword").value;
+
+var appendError= "";
+if(txtEmail == ""){
+	appendError = "Email";
+
+}
+
+if(txtUn == ""){
+	if(appendError != ""){
+	appendError = appendError+" and ";	
+	}
+	appendError = appendError+" Username ";
+}
+if(txtPassword == ""){
+	if(appendError != ""){
+	appendError = appendError+" and ";	
+	}
+	appendError = appendError+" Password ";
+
+}
+
+if(appendError != ""){
+	appendError = appendError+" can not be empty.";	
+	alert(appendError);
+	return false;
+	}
+else{
+	return true;
+}
+}
